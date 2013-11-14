dd594901d36c60673408db6e11789ac9980685dc
wuminzhe <wuminzhe@gmail.com>
2013-11-14 17:19:21 +0800

    退出signout的动作改为get，以方便调试

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Book::Application.routes.draw do
   resources :users, only: [:index, :show]
   resources :sessions, only: [:new, :create, :destroy]
   match '/signin', to: 'sessions#new', via: 'get'
-  match '/signout', to: 'sessions#destroy', via: 'delete'
+  match '/signout', to: 'sessions#destroy', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
