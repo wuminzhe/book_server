e470a5d0bdbde0c62f76aff3d83b7c8b266d4003
wuminzhe <wuminzhe@gmail.com>
2013-12-09 11:17:55 +0800

    delete some not use files

--- app/assets/javascripts/models/activity.js
+++
@@ -1,9 +0,0 @@
-var app = app || {};
-
-app.Activity = Backbone.Model.extend({
-  defaults: {
-    coverImage: 'http://pic.52mxp.com/site/text.png',
-    title: '未命名',
-    createDate: 'Unknown'
-  }
-});
